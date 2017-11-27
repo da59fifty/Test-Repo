8e8da3955b7de745c7e2f1961c95de7e0a6c4b30
da59fifty <31520636+da59fifty@users.noreply.github.com>
2017-11-27 17:20:50 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,192 +5,187 @@
 #include <cstring>
 #include <stdio.h>
 #include <stdlib.h>
+#include <cstdlib>
 #include <math.h>
 using namespace std;
-struct headNode
+struct node
 {
-    headNode * left_Child;
-    headNode * right_Child;
-    float freq;
+    node * leftChild;
+    node * rightChild;
+    double frequency;
     string content;
     string code;
 };
+int t=0;
+string codes[50];
+string letters[50];
+vector<node> nodeArray;// Use nodeArray to record all the nodes that may be created in the whole process
 
-
-string codes[52];
-string letters[52];
-vector<headNode> treeArr;
-
-headNode getLower()
+node extractMin()
 {
-    float holder = 500000;
-    vector<headNode>::iterator comparator,position;
-    for(comparator = treeArr.begin();comparator!=treeArr.end();comparator++)
+    double temp = (double) INT_MAX;
+    vector<node>::iterator i1,pos;
+    for(i1 = nodeArray.begin();i1!=nodeArray.end();i1++)
     {
-        if(holder > (*comparator).freq)
+        if(temp>(*i1).frequency)
         {
-            position = comparator;
-            holder = (*comparator).freq;
+            pos = i1;
+            temp = (*i1).frequency;
         }
     }
-    headNode holdingNode = (*position);
-    treeArr.erase(position);
-    return holdingNode;
+    node tempNode = (*pos);
+    nodeArray.erase(pos);
+    return tempNode;
 }
-
-headNode buildHTree()
+node getHuffmanTree()
 {
-    while(!treeArr.empty())
+    while(!nodeArray.empty())
     {
-        int sizeH = 1;
-        headNode * holdingNode = new headNode;
-        headNode * holdingNodeDuo = new headNode;
-        headNode * holdingNodeTrio = new headNode;
-
-        *holdingNodeDuo = getLower();
-        *holdingNodeTrio = getLower();
-
-        holdingNode->left_Child = holdingNodeDuo;
-        holdingNode->right_Child = holdingNodeTrio;
-        holdingNode->freq = (holdingNodeDuo->freq) + (holdingNodeTrio->freq);
-
-        treeArr.push_back(*holdingNode);
-        if(treeArr.size() == sizeH)
+        node * tempNode = new node;
+        node * tempNode1 = new node;
+        node * tempNode2 = new node;
+        *tempNode1 = extractMin();
+        *tempNode2 = extractMin();
+        
+        tempNode->leftChild = tempNode1;
+        tempNode->rightChild = tempNode2;
+        tempNode->frequency = tempNode1->frequency+tempNode2->frequency;
+        nodeArray.push_back(*tempNode);
+        if(nodeArray.size() == 1)//only the root node exsits
         {
             break;
         }
     }
-    return treeArr[0];
+    return nodeArray[0];
 }
 
-int orderOfLetters=0;
-void binaryBuilder(headNode * hold,string stringArr)
+void BFS(node * temproot,string s)
 {
-    headNode * root1 = new headNode;
-    root1 = hold;
-    root1->code = stringArr;
+    node * root1 = new node;
+    root1 = temproot;
+    root1->code = s;
     if(root1 == NULL)
     {
     }
-    else if(root1->left_Child == NULL && root1->right_Child == NULL)
+    else if(root1->leftChild == NULL && root1->rightChild == NULL)
     {
-        letters[orderOfLetters] = root1->content;
-        codes[orderOfLetters] = root1->code;
-        orderOfLetters++;
+        letters[t] = root1->content;
+        codes[t] = root1->code;
+        t++;
     }
     else
     {
-        root1->left_Child->code = stringArr.append("1");
-        stringArr.erase(stringArr.end()-1);
-        root1->right_Child->code = stringArr.append("0");
-        stringArr.erase(stringArr.end()-1);
-        binaryBuilder(root1->left_Child,stringArr.append("1"));
-        stringArr.erase(stringArr.end()-1);
-        binaryBuilder(root1->right_Child,stringArr.append("0"));
-        stringArr.erase(stringArr.end()-1);
+        root1->leftChild->code = s.append("1");
+        s.erase(s.end()-1);
+        root1->rightChild->code = s.append("0");
+        s.erase(s.end()-1);
+        
+        BFS(root1->leftChild,s.append("1"));
+        s.erase(s.end()-1);
+        BFS(root1->rightChild,s.append("0"));
+        s.erase(s.end()-1);
     }
+    //cout<<letters[0]<<" "<<codes[0]<<endl;
 }
-
+//sort letter and code array
 void sortByLetter()
 {
     int i,j;
-    string holder;
-    for(i=0;i<orderOfLetters-1;i++)
+    string x;
+    for(i=0;i<t-1;i++)
     {
-        for(j=0;j<orderOfLetters-i-1;j++)
+        for(j=0;j<t-i-1;j++)
         {
             if(letters[j]>letters[j+1])
             {
-                holder=letters[j];
+                x=letters[j];
                 letters[j]=letters[j+1];
-                letters[j+1]=holder;
-                holder=codes[j];
+                letters[j+1]=x;
+                
+                x=codes[j];
                 codes[j]=codes[j+1];
-                codes[j+1]=holder;
+                codes[j+1]=x;
             }
         }
     }
 }
-
-void huffBinCode()
+void getHuffmanCode()
 {
-    int i;
-    float freqHolder;
-    string letterHolder = "";
-
-    ifstream myFile;
-    myFile.open("a4-1.txt");
-
-    string stringOfInt;
-    getline(myFile, stringOfInt);
-    int intOfString = 0;
-
-    for (i = 0; i < stringOfInt.size(); i++)
-    {
-        intOfString += (stringOfInt[i] - 48) * pow(10, (stringOfInt.size() - i - 1));
+    int size,i;
+    double tempDouble;
+    string tempString = "";
+    ifstream file;
+    file.open("a4-1.txt");
+    string dummyLine;
+    getline(file, dummyLine);
+    //cout<<dummyLine;
+    //-----------------------------------------------------------String to Int
+    int result = 0;
+    for (i = 0; i < dummyLine.size(); i++) {
+        result += (dummyLine[i] - 48) * pow(10, (dummyLine.size() - i - 1));
+        //cout<<result<<endl;
     }
-
+    //--------------------------------------------------------------
     cout<<"Frequencies are:"<<endl;
-    for(i = 0;i<intOfString;i++)
+    for(i = 0;i<result;i++)
     {
-        headNode holdingNode;
-        myFile >> letterHolder >> freqHolder;
-        cout << letterHolder << " " << freqHolder<<endl;
-
-        holdingNode.content = letterHolder;
-        holdingNode.freq = freqHolder;
-        holdingNode.left_Child = 0;
-        holdingNode.right_Child = 0;
-        treeArr.push_back(holdingNode);
+        //cout<<"please input the things you want to encoded and their frequencies!"<<endl;
+        node tempNode;
+        file >> tempString >> tempDouble;
+        cout << tempString << " " << tempDouble<<endl;
+        tempNode.frequency = tempDouble;
+        tempNode.content = tempString;
+        tempNode.leftChild = NULL;
+        tempNode.rightChild = NULL;
+        nodeArray.push_back(tempNode);
     }
-
-    cout<<endl<<endl;
-
-    headNode build = buildHTree();
-    binaryBuilder(&build,"");
-    sortByLetter();
-
+    cout<<endl;
+    node root = getHuffmanTree();
+    
+    BFS(&root,"");
+    sortByLetter();//sort letter and codes in alphabetical order
+    //Printing Codes
     cout<<"Huffman Codes are:"<<endl;
-
-    for(i=0;i<orderOfLetters;i++)
+    for(i=0;i<t;i++)
     {
         cout<<letters[i]<<" "<<codes[i]<<endl;
     }
-
-    myFile.close();
+    file.close();
 }
 
 int main()
 {
-    huffBinCode();
-    string stringHolder;
+    getHuffmanCode();
+    string temp = "holder";
     char chars[52];
-    strcpy(chars,stringHolder.c_str());
-
+    strcpy(chars,temp.c_str());
+    //--------------------Read in bit file from a4-2.txt
     ifstream file2;
     file2.open("a4-2.txt");
     string binaryLine;
     getline(file2, binaryLine);
-
-    string decodedString;
-    int i;
-
+    cout<<"Encoded Data is : \n"<<binaryLine<<endl;//Print encoded data
+    //------------------------------Decode binary file
+    string plain = ""; //take empty string
+    int i; //to iterate
+    //loop till binaryLine length is not zero
     while(binaryLine.length()>0)
     {
-
-        for(i=0;i<orderOfLetters;i++)
+        //check which symbol is matching
+        for(i=0;i<t;i++)//t is number of tuples from file
         {
-            int n = codes[i].length();
-
+            int n = codes[i].length(); //take length
+            //if substring of length codes[i] is equals to codes[i], then append symbol to plain
             if(binaryLine.substr(0,n)==codes[i])
             {
-                decodedString = decodedString + letters[i];
-                binaryLine.erase (0,n);
-                break;
+                plain = plain + letters[i];
+                binaryLine.erase (0,n); //remove that substring
+                break; //break loop
             }
         }
     }
-
-    cout<<endl<<"Decoded Binary Phrase: "<<decodedString<<endl;
+    //print decoded value
+    cout<<"Decoded value: "<<plain<<endl;
+    
     return 0;
 }
